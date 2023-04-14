d9cdea5d9e51a66b1d960553311ad3c6ed0c9e10
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-04-14 12:20:14 +0300

    Initial setup

---
+++ config/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include
+
+api_urls = []
+
+urlpatterns = [
+    path('api/v1/', include(api_urls)),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(path('admin/', admin.site.urls))
